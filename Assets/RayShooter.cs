966ce7253fb181a410fe6f7b37ad74331aa58594
ChadMaass <117474788+ChadMaass@users.noreply.github.com>
2024-02-03 18:58:57 -0700

    unity project added?

---
+++ Assets/RayShooter.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+{
+    private Camera cam;
+    RaycastHit hit;
+    Vector3 hitPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(cam.pixelWidth / 2, cam.pixelHeight / 2, 0);
+            Ray ray = cam.ScreenPointToRay(point);
+            
+            if (Physics.Raycast(ray, out hit))
+            {
+                StartCoroutine(SphereIndicator(hit.point));
+                hitPoint = hit.point;
+                // GameObject hitObject = hit.transform.gameObject;
+                // ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
+                // if (target != null)
+                // {
+                //     Debug.Log("Target hit");
+                //     target.ReactToHit();
+                // }
+                // else
+                // {
+                //     StartCoroutine(SphereIndicator(hit.point));
+                // }
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        int size = 12;
+        float posX = cam.pixelWidth / 2 - size / 4;
+        float posY = cam.pixelHeight / 2 - size / 2;
+
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 30; // Increase this value to make the "+" bigger
+
+        GUIStyle style2 = new GUIStyle();
+        style2.fontSize = 20;
+
+        GUI.Label(new Rect(posX, posY, size, size), "+", style);
+        GUI.Label(new Rect(10, 10, 200, 20), "Hit Point: " + hitPoint.ToString(), style2);
+    }
+
+    private IEnumerator SphereIndicator(Vector3 pos)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+        yield return new WaitForSeconds(1);
+        Destroy(sphere);
+    }
+}
